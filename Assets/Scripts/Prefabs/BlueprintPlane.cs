8f3fd1b453b4746c02f8c808890be95f5b5f29ed
Stephen Olah <solah1701@gmail.com>
2017-11-25 22:19:58 +1100

    Added Zooming and Panning

--- Assets/Scripts/Prefabs/BlueprintPlane.cs
+++ Assets/Scripts/Prefabs/BlueprintPlane.cs
@@ -25,7 +25,51 @@ public class BlueprintPlane : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		var myTouch = Input.GetTouch (0);
+		if (Input.touchCount == 1)
+			Pan (myTouch.deltaPosition);
+		if (Input.touchCount == 2)
+			Zoom ();
+	}
+
+	private float prevMagnitude = 0;
+
+	void Zoom()
+	{
+		var touch0 = Input.GetTouch (0);
+		var touch1 = Input.GetTouch (1);
+		var touch0delta = touch0.deltaPosition;
+		var touch1delta = touch1.deltaPosition;
+		var touch0pos = touch0.position;
+		var touch1pos = touch1.position;
+		var origVector = touch0pos - touch0delta - touch1pos - touch1delta;
+		var diff = touch0.phase == TouchPhase.Began || touch1.phase == TouchPhase.Began ? 0 : origVector.magnitude - prevMagnitude;
+		Debug.Log(string.Format("Zoom Diff: {0} Magnitude: {1} origMag: {2} phase {3}", diff, origVector.magnitude, prevMagnitude, touch0.phase));
+		prevMagnitude = origVector.magnitude;
+		if (touch0.phase == TouchPhase.Ended || touch1.phase == TouchPhase.Ended) {
+			prevMagnitude = 0;
+			return;
+		}
+		Zoom (diff);
+	}
+
+	void Zoom(float factor)
+	{
+		if (factor < 0.1)
+			return;
+		const float scale = 120;
+		var zoomFactor = factor / scale;
+		var ratio = transform.localScale.x / transform.localScale.z;
+		if (transform.localScale.x + zoomFactor < 0 || transform.localScale.z + zoomFactor < 0)
+			return;
+		transform.localScale += new Vector3 (zoomFactor * ratio, 0, zoomFactor);
+	}
+
+	void Pan(Vector2 position)
+	{ 
+		const float scale = 60;
+		transform.Translate(-position.x / scale, 0, -position.y / scale);
 	}
 }
