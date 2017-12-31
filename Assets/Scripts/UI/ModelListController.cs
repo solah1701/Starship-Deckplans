d6ff5c65c02e0642584a0a9c3b396144e1461dd7
Stephen Olah <solah1701@gmail.com>
2017-12-31 13:57:36 +1100

    Added a Model List controller

---
+++ Assets/Scripts/UI/ModelListController.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Models;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelListController : MonoBehaviour {
+
+    public GameObject PrefabItem;
+    public RectTransform BlueprintPanel;
+    public ShipManager ShipManager;
+
+    private ModelMesh Mesh;
+    public ModelMesh Current { get; set; }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
