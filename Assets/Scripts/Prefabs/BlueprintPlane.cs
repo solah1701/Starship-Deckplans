9f1ddfed0d246072eaca6ebb19b6d0ef84cc2ba2
Stephen Olah <solah1701@gmail.com>
2017-12-31 20:17:09 +1100

    Added a generic Zoom and Pan and refactored the bluerprint plane to use it

--- Assets/Scripts/Prefabs/BlueprintPlane.cs
+++ Assets/Scripts/Prefabs/BlueprintPlane.cs
@@ -4,15 +4,17 @@ using System.IO;
 using UnityEngine;
 using Assets.Scripts.Extenders;
 
+[RequireComponent(typeof(ZoomAndPan))]
 public class BlueprintPlane : MonoBehaviour
 {
-
     private float _prevMagnitude = 0;
+    private ZoomAndPan _zoomAndPan;
     private float _scaleX;
     private Blueprint _currentBlueprint;
 
     public IEnumerator SetBlueprintItem(Blueprint item)
     {
+        _zoomAndPan = GetComponent<ZoomAndPan>();
         _currentBlueprint = item;
         var filename = Path.Combine(item.FilePath, item.FileName);
         yield return StartCoroutine(SetTexture(filename));
@@ -40,56 +42,17 @@ public class BlueprintPlane : MonoBehaviour
 		_scaleX = (width / height) - 1F;
 		renderer.material.mainTexture = www.texture;
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		if (Input.touchCount == 1)
-			Pan ();
-		if (Input.touchCount == 2)
-			Zoom ();
-	}
 
-	void Zoom()
-	{
-		var touch0 = Input.GetTouch (0);
-		var touch1 = Input.GetTouch (1);
-		var touch0Delta = touch0.deltaPosition;
-		var touch1Delta = touch1.deltaPosition;
-		var touch0Pos = touch0.position;
-		var touch1Pos = touch1.position;
-		var origVector = touch0Pos - touch0Delta - touch1Pos - touch1Delta;
-		var diff = touch0.phase == TouchPhase.Began || touch1.phase == TouchPhase.Began ? 0 : origVector.magnitude - _prevMagnitude;
-		_prevMagnitude = origVector.magnitude;
-		if (touch0.phase == TouchPhase.Ended || touch1.phase == TouchPhase.Ended) {
-			_prevMagnitude = 0;
-			return;
-		}
-		Zoom (diff);
-	}
-
-	void Zoom(float factor)
-	{
-		const float scale = 120;
-		float n = 1.0f;
-		if (transform.localScale.x > 3.0f)
-			n = 3.0f;
-		var zoomFactor = factor / (scale / n );
-		var ratio = transform.localScale.x / transform.localScale.z;
-		if (transform.localScale.x + zoomFactor < 0 || transform.localScale.z + zoomFactor < 0)
-			return;
-		transform.localScale += new Vector3 (zoomFactor * ratio, 0, zoomFactor);
-		Debug.Log (string.Format ("Zoom factor: {0} x: {1} z: {2}", zoomFactor, transform.localScale.x, transform.localScale.z));
-        _currentBlueprint.Scale = transform.localScale.Map();
-
-	}
-
-	void Pan()
-	{
-        var myTouch = Input.GetTouch(0);
-	    Vector2 position = myTouch.deltaPosition;
-        const float scale = 60;
-		transform.Translate(-position.x / scale, 0, -position.y / scale);
-        _currentBlueprint.Position = transform.position.Map();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1)
+            _currentBlueprint.Position = _zoomAndPan.Pan().Map();
+        if (Input.touchCount == 2)
+        {
+            var theScale = _zoomAndPan.Zoom();
+            if (theScale != Vector3.zero) _currentBlueprint.Scale = theScale.Map();
+        }
+        Debug.Log(string.Format("Zoom x: {0} z: {1}", _currentBlueprint.Scale.x, _currentBlueprint.Scale.z));
+    }
 }
---
+++ Assets/Scripts/Prefabs/ZoomAndPan.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Extenders;
+using UnityEngine;
+
+public class ZoomAndPan : MonoBehaviour {
+
+    private float _prevMagnitude = 0;
+
+    public Vector3 Zoom()
+    {
+        var touch0 = Input.GetTouch(0);
+        var touch1 = Input.GetTouch(1);
+        var touch0Delta = touch0.deltaPosition;
+        var touch1Delta = touch1.deltaPosition;
+        var touch0Pos = touch0.position;
+        var touch1Pos = touch1.position;
+        var origVector = touch0Pos - touch0Delta - touch1Pos - touch1Delta;
+        var diff = touch0.phase == TouchPhase.Began || touch1.phase == TouchPhase.Began ? 0 : origVector.magnitude - _prevMagnitude;
+        _prevMagnitude = origVector.magnitude;
+        if (touch0.phase != TouchPhase.Ended && touch1.phase != TouchPhase.Ended) return Zoom(diff);
+        _prevMagnitude = 0;
+        return new Vector3();
+    }
+
+    public Vector3 Zoom(float factor)
+    {
+        const float scale = 120;
+        float n = 1.0f;
+        if (transform.localScale.x > 3.0f)
+            n = 3.0f;
+        var zoomFactor = factor / (scale / n);
+        var ratio = transform.localScale.x / transform.localScale.z;
+        if (transform.localScale.x + zoomFactor < 0 || transform.localScale.z + zoomFactor < 0)
+            return new Vector3();
+        transform.localScale += new Vector3(zoomFactor * ratio, 0, zoomFactor);
+        return transform.localScale;
+    }
+
+    public Vector3 Pan()
+    {
+        var myTouch = Input.GetTouch(0);
+        Vector2 position = myTouch.deltaPosition;
+        const float scale = 60;
+        transform.Translate(-position.x / scale, 0, -position.y / scale);
+        return transform.position;
+    }
+}
