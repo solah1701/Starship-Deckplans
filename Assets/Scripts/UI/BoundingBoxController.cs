24813d126be4fda9bb33d02c254a7f4c82c9c236
Stephen Olah <solah1701@gmail.com>
2018-01-14 12:27:56 +1100

    Made changes to bounding box controller to capture the touch input properly

--- Assets/Scripts/UI/BoundingBoxController.cs
+++ Assets/Scripts/UI/BoundingBoxController.cs
@@ -7,6 +7,11 @@ public class BoundingBoxController : MonoBehaviour {
     public BoundingBox BoundingBoxPrefab;
     public ObjectModelManager ObjectModelManager;
 
+    private BoundingBox _prefab;
+	private bool _boxStarted;
+    private Vector2 _startPosition;
+    private Vector2 _endPosition;
+
     // Use this for initialization
     void Start () {
 		
@@ -15,6 +20,38 @@ public class BoundingBoxController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (ObjectModelManager.SelectVertexMode)
-            Debug.LogWarning("Vertex Mode");
+            SetupBoundingBox();
+	}
+
+    void SetupBoundingBox()
+    {
+		if (Input.touchCount < 1)
+			return;
+        var touch = Input.GetTouch(0);
+		if (!_boxStarted && touch.phase == TouchPhase.Began) {
+			_startPosition = touch.position;
+			CreatePrefab ();
+			_boxStarted = true;
+			Debug.Log(string.Format("Bounding Box: Start position {0}", _startPosition));
+		}
+        if (_boxStarted && touch.phase == TouchPhase.Ended)
+        {
+            _endPosition = touch.position;
+			_boxStarted = false;
+			Debug.Log(string.Format("Bounding Box: End position {0}", _endPosition));
+        }
+    }
+
+    void CreatePrefab()
+    {
+		DestroyPrefab ();
+        _prefab = Instantiate(BoundingBoxPrefab);
+        _prefab.transform.SetParent(transform, true);
+    }
+
+	void DestroyPrefab(){
+		if (_prefab == null)
+			return;
+		Destroy (_prefab);
 	}
 }
