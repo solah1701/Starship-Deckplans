52af671293d55bf503d4355de7a8556c84f26fd4
Stephen Olah <solah1701@gmail.com>
2018-01-25 22:53:37 +1100

    removed the debug code

--- Assets/Scripts/Experiments/Cube.cs
+++ Assets/Scripts/Experiments/Cube.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class Cube : MonoBehaviour {
 
     public int xSize, ySize, zSize;
-    public int quads, ring, t, v;
+    public int roundness;
+
+    private int quads, ring, t, v;
     private Mesh mesh;
     private Vector3[] vertices;
+    private Vector3[] normals;
 
     private void Awake()
     {
@@ -33,29 +36,24 @@ public class Cube : MonoBehaviour {
             (xSize - 1) * (zSize - 1) +
             (ySize - 1) * (zSize - 1)) * 2;
         vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
-        Debug.Log(string.Format("Corner Verts {0}, Edge Verts {1}, Face Verts {2}, Total Verts {3}", cornerVertices, edgeVertices, faceVertices, vertices.Length));
         int v = 0;
         for (int y = 0; y <= ySize; y++)
         {
             for (int x = 0; x <= xSize; x++)
             {
                 vertices[v++] = new Vector3(x, y, 0);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, x, y, 0));
             }
             for (int z = 1; z <= zSize; z++)
             {
                 vertices[v++] = new Vector3(xSize, y, z);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, xSize, y, z));
             }
             for (int x = xSize - 1; x >= 0; x--)
             {
                 vertices[v++] = new Vector3(x, y, zSize);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, x, y, zSize));
             }
             for (int z = zSize - 1; z > 0; z--)
             {
                 vertices[v++] = new Vector3(0, y, z);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, 0, y, z));
             }
         }
         for (int z = 1; z < zSize; z++)
@@ -63,7 +61,6 @@ public class Cube : MonoBehaviour {
             for (int x = 1; x < xSize; x++)
             {
                 vertices[v++] = new Vector3(x, ySize, z);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, x, ySize, z));
             }
         }
         for (int z = 1; z < zSize; z++)
@@ -71,7 +68,6 @@ public class Cube : MonoBehaviour {
             for (int x = 1; x < xSize; x++)
             {
                 vertices[v++] = new Vector3(x, 0, z);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, x, 0, z));
             }
         }
         mesh.vertices = vertices;
@@ -92,7 +88,6 @@ public class Cube : MonoBehaviour {
             }
             t = SetQuad(triangles, t, v, v + 1, v - ring + 1, v + ring + 1);
         }
-        Debug.Log(string.Format("mesh triangles {0}, triangles {1}", mesh.triangles.Length, triangles.Length));
         t = CreateTopFace(triangles, t, ring);
         t = CreateBottomFace(triangles, t, ring);
         mesh.triangles = triangles;
@@ -190,32 +185,27 @@ public class Cube : MonoBehaviour {
             (xSize - 1) * (zSize - 1) +
             (ySize - 1) * (zSize - 1)) * 2;
         vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
-        Debug.Log(string.Format("Corner Verts {0}, Edge Verts {1}, Face Verts {2}, Total Verts {3}", cornerVertices, edgeVertices, faceVertices, vertices.Length));
         int v = 0;
         for (int y = 0; y <= ySize; y++)
         {
             for (int x = 0; x <= xSize; x++)
             {
                 vertices[v++] = new Vector3(x, y, 0);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, x, y, 0));
                 yield return wait;
             }
             for (int z = 1; z <= zSize; z++)
             {
                 vertices[v++] = new Vector3(xSize, y, z);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, xSize, y, z));
                 yield return wait;
             }
             for (int x = xSize - 1; x >= 0; x--)
             {
                 vertices[v++] = new Vector3(x, y, zSize);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, x, y, zSize));
                 yield return wait;
             }
             for (int z = zSize - 1; z > 0; z--)
             {
                 vertices[v++] = new Vector3(0, y, z);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, 0, y, z));
                 yield return wait;
             }
         }
@@ -224,7 +214,6 @@ public class Cube : MonoBehaviour {
             for (int x = 1; x < xSize; x++)
             {
                 vertices[v++] = new Vector3(x, ySize, z);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, x, ySize, z));
                 yield return wait;
             }
         }
@@ -233,7 +222,6 @@ public class Cube : MonoBehaviour {
             for (int x = 1; x < xSize; x++)
             {
                 vertices[v++] = new Vector3(x, 0, z);
-                Debug.Log(string.Format("Vertice {0}: x {1}, y {2}, z {3}", v, x, 0, z));
                 yield return wait;
             }
         }
