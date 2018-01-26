ca3dce5d33900ef3a4c8312497d5feea94128376
Stephen Olah <solah1701@gmail.com>
2018-01-26 17:57:54 +1100

    Generated a rounded Cube

--- Assets/Scripts/Experiments/RoundedCube.cs
+++ Assets/Scripts/Experiments/RoundedCube.cs
@@ -42,57 +42,86 @@ public class RoundedCube : MonoBehaviour {
         {
             for (int x = 0; x <= xSize; x++)
             {
-                vertices[v++] = new Vector3(x, y, 0);
+                SetVertex(v++, x, y, 0);
             }
             for (int z = 1; z <= zSize; z++)
             {
-                vertices[v++] = new Vector3(xSize, y, z);
+                SetVertex(v++, xSize, y, z);
             }
             for (int x = xSize - 1; x >= 0; x--)
             {
-                vertices[v++] = new Vector3(x, y, zSize);
+                SetVertex(v++, x, y, zSize);
             }
             for (int z = zSize - 1; z > 0; z--)
             {
-                vertices[v++] = new Vector3(0, y, z);
+                SetVertex(v++, 0, y, z);
             }
         }
         for (int z = 1; z < zSize; z++)
         {
             for (int x = 1; x < xSize; x++)
             {
-                vertices[v++] = new Vector3(x, ySize, z);
+                SetVertex(v++, x, ySize, z);
             }
         }
         for (int z = 1; z < zSize; z++)
         {
             for (int x = 1; x < xSize; x++)
             {
-                vertices[v++] = new Vector3(x, 0, z);
+                SetVertex(v++, x, 0, z);
             }
         }
         mesh.vertices = vertices;
         mesh.normals = normals;
     }
 
+    private void SetVertex(int i, int x, int y, int z)
+    {
+        Vector3 inner = vertices[i] = new Vector3(x, y, z);
+        if (x < roundness) inner.x = roundness;
+        else if (x > xSize - roundness) inner.x = xSize - roundness;
+        if (y < roundness) inner.y = roundness;
+        else if (y > ySize - roundness) inner.y = ySize - roundness;
+        if (z < roundness) inner.z = roundness;
+        else if (z > zSize - roundness) inner.z = zSize - roundness;
+        normals[i] = (vertices[i] - inner).normalized;
+        vertices[i] = inner + normals[i]*roundness;
+    }
+
     private void CreateTriangles()
     {
-        quads = (xSize * ySize + xSize * zSize + ySize * zSize) * 2;
-        int[] triangles = new int[quads * 6];
-        ring = (xSize + zSize) * 2;
-        t = 0;
+        int[] trianglesZ = new int[(xSize*ySize)*12];
+        int[] trianglesX = new int[(ySize*zSize)*12];
+        int[] trianglesY = new int[(xSize*zSize)*12];
+        ring = (xSize + zSize)*2;
+        int tZ = 0, tX = 0, tY = 0;
         v = 0;
         for (int y = 0; y < ySize; y++, v++)
         {
-            for (int q = 0; q < ring - 1; q++, v++)
+            for (int q = 0; q < xSize; q++, v++)
             {
-                t = SetQuad(triangles, t, v, v + 1, v + ring, v + ring + 1);
+                tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
             }
-            t = SetQuad(triangles, t, v, v + 1, v - ring + 1, v + ring + 1);
+            for (int q = 0; q < zSize; q++, v++)
+            {
+                tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
+            }
+            for (int q = 0; q < xSize; q++, v++)
+            {
+                tZ = SetQuad(trianglesZ, tZ, v, v + 1, v + ring, v + ring + 1);
+            }
+            for (int q = 0; q < zSize - 1; q++, v++)
+            {
+                tX = SetQuad(trianglesX, tX, v, v + 1, v + ring, v + ring + 1);
+            }
+            tX = SetQuad(trianglesX, tX, v, v + 1, v - ring + 1, v + ring + 1);
         }
-        t = CreateTopFace(triangles, t, ring);
-        t = CreateBottomFace(triangles, t, ring);
-        mesh.triangles = triangles;
+        tY = CreateTopFace(trianglesY, tY, ring);
+        tY = CreateBottomFace(trianglesY, tY, ring);
+        mesh.subMeshCount = 3;
+        mesh.SetTriangles(trianglesZ, 0);
+        mesh.SetTriangles(trianglesX, 1);
+        mesh.SetTriangles(trianglesY, 2);
     }
 
     private int CreateTopFace(int[] triangles, int t, int ring)
@@ -166,7 +195,7 @@ public class RoundedCube : MonoBehaviour {
 
     private static int SetQuad(int[] triangles, int i, int v00, int v10, int v01, int v11)
     {
-        Debug.Log(string.Format("v00 {0}, v01, {1}, v10 {2}, v11 {3}, index {4}", v00, v01, v10, v11, i));
+        //Debug.Log(string.Format("v00 {0}, v01, {1}, v10 {2}, v11 {3}, index {4}", v00, v01, v10, v11, i));
         triangles[i] = v00;
         triangles[i + 1] = triangles[i + 4] = v01;
         triangles[i + 2] = triangles[i + 3] = v10;
@@ -234,10 +263,10 @@ public class RoundedCube : MonoBehaviour {
         if (vertices == null) return;
         for(int i = 0; i < vertices.Length; i++)
         {
-            Gizmos.color = Color.black;
+            Gizmos.color = Color.green;
             Gizmos.DrawSphere(vertices[i], 0.1f);
-            //Gizmos.color = Color.yellow;
-            //Gizmos.DrawRay(vertices[i], normals[i]);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(vertices[i], normals[i]);
         }
     }
 }
